485787826ede4acb743ab9e4266b0d0b64f5b126
Abin <abinth250@gmail.com>
2023-07-14 18:23:21 +0530

    login page designs and validations completed

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+function App() {
+
+  return (
+   <div >
+    Home page
+    <Outlet/>
+   </div>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
