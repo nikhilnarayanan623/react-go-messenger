646d6aa2054c96124a483df2fb7bbbe23e9ee7ad
nikhilnarayanan623 <nikhilnarayanan623@gmail.com>
2023-07-16 18:13:53 +0530

    authorization header added on cors

--- server/pkg/api/middleware/cors.go
+++ server/pkg/api/middleware/cors.go
@@ -10,7 +10,7 @@ func (c *middleware) Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Type"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
 
 	return cors.New(config)
 }
